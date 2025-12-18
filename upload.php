302673c6f1e854af60864e026a924f2f6e8eebc4
Poommin.tk <poommin.tk@gmail.com>
2025-12-18 14:09:30 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,6 +1,13 @@
+<style>
+        body {
+            background:#ffffff;
+            font-family: Arial;
+        }
+</style>
+
 <?php
 $target_dir = "uploads/"; // โฟลเดอร์เก็บไฟล์
-$target_file = $target_dir . basename($_FILES["file1"]["name"]);
+$target_file = $target_dir . basename($_FILES["file"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
@@ -10,7 +17,7 @@ if (file_exists($target_file)) {
     $uploadOk = 0;
 }
 
-if ($_FILES["file1"]["size"] > 51200) {
+if ($_FILES["file"]["size"] > 51200) {
     echo "ไฟล์มีขนาดเกิน 50 KB<br>";
     $uploadOk = 0;
 }
@@ -30,19 +37,19 @@ if ($uploadOk == 0) {
     echo "ไม่สามารถอัปโหลดไฟล์ได้";
 } else {
 
-    if (move_uploaded_file($_FILES["file1"]["tmp_name"], $target_file)) {
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
 
         echo "อัปโหลดไฟล์สำเร็จ<br><br>";
 
 
-        echo "File Name : " . basename($_FILES["file1"]["name"]) . "<br>";
+        echo "File Name : " . basename($_FILES["file"]["name"]) . "<br>";
         echo "File Type : " . $imageFileType . "<br>";
-        echo "File Size : " . $_FILES["file1"]["size"] . " bytes<br>";
+        echo "File Size : " . $_FILES["file"]["size"] . " bytes<br>";
         echo "File Path : " . $target_file . "<br><br>";
 
 
         if ($imageFileType == "gif" || $imageFileType == "jpeg") {
-            echo "<img src='$target_file' width='300'>";
+            echo "<img src='$target_file' width='300px'>";
         }
 
     } else {
