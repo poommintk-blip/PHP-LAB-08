05b0bb1a120273d39c3642f35a3f7f99c2ac07c9
Poommin.tk <poommin.tk@gmail.com>
2025-12-18 14:57:45 +0700

    Update include.php

--- include.php
+++ include.php
@@ -1,11 +1,9 @@
 <html>
 <body>
 <!-- สร้าง include -->
-<a href="http://www.sut.ac.th/">SUT</a> |
-<a href="http://ceit.sut.ac.th/">CEIT</a> |
-<a href="https://www.facebook.com/poommin.tikumrum/">Facebook</a> |
-<a href="https://www.instagram.com/poommin_2607/">Instagram</a> |
-<a href="contact.php">Contact Us</a>
- 
+ <?php 
+ include("home2.php");
+ ?>
+
 </body>
 </html>
