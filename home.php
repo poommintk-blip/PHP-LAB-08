cac599aef264b7044a01d017574fcbb858ece18c
Poommin.tk <poommin.tk@gmail.com>
2025-12-18 13:17:41 +0700

    Update home.php

--- home.php
+++ home.php
@@ -13,7 +13,7 @@
 </head>
 
 <!-- ใส่รูปภาพพื้นหลัง -->
-<body style="background-image: ('/.bg13.jpg'); background-size: cover; background-attachment: fixed;">
+<body style="background-image: ('\bg13.jpg'); background-size: cover; background-attachment: fixed;">
     <?php include("menu.php"); ?>
     <div class="row">
         <div class="column">
@@ -39,11 +39,11 @@
 		<div class="column">
             <!-- ใส่ลิงก์ในหน้าเว็บที่นี่ -->
             <div class="column-right">
-                <p><a href="">Location 1</a> | <br>
-                <a href="">Location 2</a> | <br>
-                <a href="">Location 3</a> | <br>
-                    <a href="">Location 4</a> | <br>
-                    <a href="">Location 5</a> | <br>
+                <p><a href="https://www.google.com/maps/place/%E0%B8%AD%E0%B8%B1%E0%B8%A5%E0%B8%9A%E0%B8%B9%E0%B9%80%E0%B8%84%E0%B8%AD%E0%B8%A3%E0%B9%8C%E0%B8%84%E0%B8%B5+%E0%B8%99%E0%B8%B4%E0%B8%A7%E0%B9%80%E0%B8%A1%E0%B9%87%E0%B8%81%E0%B8%8B%E0%B8%B4%E0%B9%82%E0%B8%81+%E0%B8%AA%E0%B8%AB%E0%B8%A3%E0%B8%B1%E0%B8%90%E0%B8%AD%E0%B9%80%E0%B8%A1%E0%B8%A3%E0%B8%B4%E0%B8%81%E0%B8%B2/@35.0823726,-106.671984,11z/data=!3m1!4b1!4m6!3m5!1s0x87220addd309837b:0xc0d3f8ceb8d9f6fd!8m2!3d35.0843859!4d-106.650422!16zL20vMGRqZDM?entry=ttu&g_ep=EgoyMDI1MTIwOS4wIKXMDSoKLDEwMDc5MjA3MUgBUAM%3D">Location 1</a> | <br>
+                <a href="https://www.google.com/maps/place/%E0%B9%80%E0%B8%81%E0%B8%AD%E0%B9%80%E0%B8%A3%E0%B9%80%E0%B8%A1+50180+G%C3%B6reme+Belediyesi%2FNev%C5%9Fehir+Merkez%2F%E0%B8%88%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B9%80%E0%B8%99%E0%B8%9F%E0%B9%80%E0%B8%8A%E0%B8%AE%E0%B8%B5%E0%B8%A3%E0%B9%8C+%E0%B8%95%E0%B8%B8%E0%B8%A3%E0%B8%81%E0%B8%B5/@38.6430719,34.8201342,15z/data=!3m1!4b1!4m5!3m4!1s0x152a6879d36bd1ab:0x466c4e17a23fae50!8m2!3d38.643056!4d34.828889">Location 2</a> | <br>
+                <a href="https://www.google.com/maps/place/%E0%B8%AD%E0%B8%B2%E0%B8%93%E0%B8%B2%E0%B8%88%E0%B8%B1%E0%B8%81%E0%B8%A3%E0%B8%9E%E0%B8%B8%E0%B8%81%E0%B8%B2%E0%B8%A1+%E0%B9%80%E0%B8%A1%E0%B8%B5%E0%B8%A2%E0%B8%99%E0%B8%A1%E0%B8%B2%E0%B8%A3%E0%B9%8C+(%E0%B8%9E%E0%B8%A1%E0%B9%88%E0%B8%B2)/@21.1722165,94.8544872,16z/data=!3m1!4b1!4m5!3m4!1s0x30b5e50393d8ca61:0x371a4717c3e89981!8m2!3d21.1717271!4d94.8585458">Location 3</a> | <br>
+                    <a href="https://www.google.com/maps/place/Ch%C3%A2teau-d'Oex,+%E0%B8%AA%E0%B8%A7%E0%B8%B4%E0%B8%95%E0%B9%80%E0%B8%8B%E0%B8%AD%E0%B8%A3%E0%B9%8C%E0%B9%81%E0%B8%A5%E0%B8%99%E0%B8%94%E0%B9%8C/@46.454216,6.9723948,11z/data=!3m1!4b1!4m5!3m4!1s0x478e8d819c0fd31b:0xa95adbf4718e3de7!8m2!3d46.4715174!4d7.1260114">Location 4</a> | <br>
+                    <a href="https://maps.app.goo.gl/K1TGXimqmkWUm2nc7">Location 5</a> | <br>
                     <a href="gallery.php">Gallery</a> | <br>
                     <a href="https://www.facebook.com/poommin.tikumrum/">Facebook</a> |
                     <a href="https://www.instagram.com/poommin_2607/">Instagram</a> |
